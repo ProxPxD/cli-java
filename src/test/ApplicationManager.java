3cef380408dca191a8e14777b622626725ce3069
ProxPxD <piotr10tutek@poczta.onet.pl>
2021-12-16 20:19:02 +0100

    Creating basis for testing

---
+++ src/test/ApplicationManager.java
@@ -0,0 +1,69 @@
+package test;
+
+import cli.Command;
+import cli.CommandLineInterface;
+
+
+public class ApplicationManager {
+
+    private boolean isWorking = false;
+    private CommandLineInterface cli;
+
+    public ApplicationManager() {
+        createCli();
+    }
+
+    public boolean isOn() {
+        return isWorking;
+    }
+
+    public void turnOn() {
+        isWorking = true;
+    }
+
+    public void turnOff() {
+        isWorking = false;
+    }
+
+    public void readInstruction() {
+        cli.readInstruction();
+    }
+
+    public void executeInstruction() {
+        cli.executeInstruction();
+    }
+
+    private void createCli() {
+        initCli();
+        initCliOptions();
+    }
+
+    private void initCli() {
+        cli = new CommandLineInterface();
+        cli.setInputStream(System.in);
+        cli.setOutputStream(System.out);
+    }
+
+    private void initCliOptions() {
+        initCliAddOption();
+        initCliSetOption();
+        initCliExitOption();
+    }
+
+    private void initCliAddOption() {
+        Command add = cli.addCommand(Constants.ADD).setDescription(Constants.ADD_DESCRIPTION);
+        Command addLang = add.addCommand(Constants.LANGUAGE, Constants.LANG).setDescription(Constants.ADD_LANGUAGE_DESCRIPTION);
+        addLang.addAction(args -> {cli.setStateValue(Constants.LANGUAGE, args[0]);});
+    }
+
+    private void initCliSetOption() {
+        Command set = cli.addCommand(Constants.SET).setDescription(Constants.SET_DESCRIPTION);
+        Command setLang = set.addCommand(Constants.LANGUAGE, Constants.LANG).setDescription(Constants.SET_LANG_DESCRIPTION);
+        setLang.addAction(args -> cli.setStateValue(Constants.LANGUAGE, args[0]));
+    }
+
+    private void initCliExitOption() {
+        Command exit = cli.addCommand(Constants.EXIT, Constants.QUIT, Constants.Q).setDescription(Constants.EXIT_DESCRIPTION);
+        exit.addAction(args -> turnOff());
+    }
+}
---
+++ src/test/Constants.java
@@ -0,0 +1,25 @@
+package test;
+
+public final class Constants {
+
+    public static final String ROOT = "root";
+
+    public static final String EXIT = "exit";
+    public static final String QUIT = "quit";
+    public static final String Q = "q";
+    public static final String ADD = "add";
+    public static final String SET = "set";
+    public static final String HELP = "help";
+
+    public static final String LANG = "lang";
+    public static final String LANGUAGE = "language";
+
+    public static final String ADD_DESCRIPTION = "Adds";
+    public static final String ADD_LANGUAGE_DESCRIPTION = "Adds a new language";
+    public static final String SET_DESCRIPTION = "Sets a current state";
+    public static final String SET_LANG_DESCRIPTION = "Sets a current language";
+    public static final String EXIT_DESCRIPTION = "Exits the application";
+
+    public static final String NON_EXISTING_COMMAND = "cli.Command %s does not exist!";
+
+}
