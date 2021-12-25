ecc8955a7f0571cff78992cd45ecb0738e4be35a
ProxPxD <piotr10tutek@poczta.onet.pl>
2021-12-25 22:03:07 +0100

    fixing constants

--- src/test/Constants.java
+++ src/test/Constants.java
@@ -12,12 +12,12 @@ public final class Constants {
     public static final String HELP = "help";
 
     public static final String LANG = "lang";
-    public static final String LANGUAGE = "Application.language";
+    public static final String LANGUAGE = "language";
 
     public static final String ADD_DESCRIPTION = "Adds";
-    public static final String ADD_LANGUAGE_DESCRIPTION = "Adds a new Application.language";
+    public static final String ADD_LANGUAGE_DESCRIPTION = "Adds a new language";
     public static final String SET_DESCRIPTION = "Sets a current state";
-    public static final String SET_LANG_DESCRIPTION = "Sets a current Application.language";
+    public static final String SET_LANG_DESCRIPTION = "Sets a current language";
     public static final String EXIT_DESCRIPTION = "Exits the application";
 
     public static final String NON_EXISTING_COMMAND = "cli.Command %s does not exist!";
